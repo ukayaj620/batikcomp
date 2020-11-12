aa23ae0a4ea34ca57d51f82de371450d4409c92b
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-12 23:21:20 +0700

    feat: create init image dimension and bitmap function

--- main.pas
+++ main.pas
@@ -5,9 +5,16 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  ExtDlgs;
 
 type
+  Channel = record
+    R, G, B: Byte;
+  end;
+  Kernel = array[1..3, 1..3] of Integer;
+  BitmapColor = array[0..1000, 0..1000] of Channel;
+  BitmapBinary = array[0..1000, 0..1000] of Boolean;
 
   { TFormMain }
 
@@ -24,7 +31,12 @@ type
     ImageTexture2: TImage;
     ImageObject: TImage;
     Image5: TImage;
+    OpenPictureDialog1: TOpenPictureDialog;
+    SavePictureDialog1: TSavePictureDialog;
+    procedure ButtonPatternClick(Sender: TObject);
   private
+    procedure InitImageDimension(imgWidth: Integer; imgHeight: Integer);
+    procedure InitImageBitmap(image: TImage; bitmap: BitmapColor);
 
   public
 
@@ -36,6 +48,55 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TFormMain }
+
+uses
+  windows;
+
+var
+  BitmapPattern, BitmapTexture1, BitmapTexture2, BitmapObject: BitmapColor;
+  BitmapBinaryImage: BitmapBinary;
+  imageWidth, imageHeight: Integer;
+
+procedure TFormMain.ButtonPatternClick(Sender: TObject);
+begin
+  if OpenPictureDialog1.Execute then
+  begin
+    ImagePattern.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+    imageWidth:= ImagePattern.Width;
+    imageHeight:= ImagePattern.Height;
+    InitImageDimension(imageWidth, imageHeight);
+
+    InitImageBitmap(ImagePattern, BitmapPattern);
+  end;
+end;
+
+procedure TFormMain.InitImageBitmap(image: TImage; bitmap: BitmapColor);
+var
+  x, y: Integer;
+begin
+  for y:= 1 to imageWidth do
+  begin
+    for x:= 1 to imageHeight do
+    begin
+      bitmap[x, y].R:= GetRValue(image.Canvas.Pixels[x-1, y-1]);
+      bitmap[x, y].G:= GetGValue(image.Canvas.Pixels[x-1, y-1]);
+      bitmap[x, y].B:= GetBValue(image.Canvas.Pixels[x-1, y-1]);
+    end;
+  end;
+end;
+
+procedure TFormMain.InitImageDimension(imgWidth: Integer; imgHeight: Integer);
+begin
+  ImageTexture1.Width:= imgWidth;
+  ImageTexture2.Width:= imgWidth;
+  ImageObject.Width:= imgWidth;
+
+  ImageTexture1.Height:= imgHeight;
+  ImageTexture2.Height:= imgHeight;
+  ImageObject.Height:= imgHeight;
+end;
 
 end.
 

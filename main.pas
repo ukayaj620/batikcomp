093e666ef23185a70c5c55a237d807a36bd4be78
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-21 21:25:30 +0700

    feat: delete sub page, and redesign main UI

--- main.pas
+++ main.pas
@@ -45,12 +45,12 @@ type
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
+    Label6: TLabel;
     OpenPictureDialog1: TOpenPictureDialog;
     PageControl: TPageControl;
     Panel1: TPanel;
     SavePictureDialog1: TSavePictureDialog;
     TabSheet1: TTabSheet;
-    TabSheet2: TTabSheet;
     TabSheet3: TTabSheet;
     procedure ButtonExecuteClick(Sender: TObject);
     procedure ButtonPattern2Click(Sender: TObject);
